dd5abc5fe85c627262c565ae6eb456056baf8003
Cristian Nestor Carle <cristiancarle@gmail.com>
2024-11-04 07:17:13 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,13 +1,22 @@
 document.getElementById("loginForm").addEventListener("submit", function(event) {
     event.preventDefault();
-    console.log("Evento de formulario capturado"); // Verifica que esto aparezca en la consola
-
+    
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
+    const modal = document.getElementById("modal");
+    const modalMessage = document.getElementById("modal-message");
 
     if (username === "admin" && password === "1234") {
-        alert("Inicio de sesión exitoso");
+        modalMessage.textContent = "Inicio de sesión exitoso";
     } else {
-        alert("Usuario o contraseña incorrectos");
+        modalMessage.textContent = "Usuario o contraseña incorrectos";
     }
+
+    // Mostrar el modal
+    modal.style.display = "flex";
+
+    // Cerrar el modal al hacer clic en el botón "Aceptar"
+    document.getElementById("modal-close").addEventListener("click", function() {
+        modal.style.display = "none";
+    });
 });
