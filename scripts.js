4dfb07f15215c1a92f0447c7dedf2cd402885c8f
Cristian Nestor Carle <cristiancarle@gmail.com>
2024-11-04 07:03:58 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,10 @@
 document.getElementById("loginForm").addEventListener("submit", function(event) {
     event.preventDefault();
-    
+    console.log("Evento de formulario capturado"); // Verifica que esto aparezca en la consola
+
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
-    // Simulación de validación
     if (username === "admin" && password === "1234") {
         alert("Inicio de sesión exitoso");
     } else {
